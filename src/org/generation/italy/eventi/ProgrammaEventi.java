f24bbea918e3ac76a0311d8bb6ad29373cdeee59
Shaw <michele_aliffi@yahoo.it>
2021-12-03 17:04:06 +0100

    Inizio Milestone 3, ma non finita

--- src/org/generation/italy/eventi/ProgrammaEventi.java
+++ src/org/generation/italy/eventi/ProgrammaEventi.java
@@ -1,5 +1,30 @@
 package org.generation.italy.eventi;
 
-public class ProgrammaEventi {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ProgrammaEventi {
+	
+	// attributi
+	String titolo;
+	List<Evento> eventi;
+	
+	// costruttore
+	public ProgrammaEventi(String titolo) {
+		this.titolo = titolo;
+		this.eventi = new ArrayList<>();
+	}
+	
+	// metodi
+	public void addEvent(List<Evento> eventi) {
+		addEvent(eventi);
+	}
+	
+	public List<Evento> allDateEvent(LocalDate d) {
+		if() {
+			
+		}
+	}
+	
 }
